6b2265e0dc8eebeb08023a7ec85b33036848d483
ch <boych.0002@gmail.com>
2025-09-21 13:11:49 +0700

    created task data

---
+++ app/components/Datacard.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const userListData = useState('userListData', () => {ref()})
+
+async function loadData() {
+ let res = await fetch('http://localhost:4000/user-list')
+ userListData.value = await res.json()
+}
+
+onMounted(async () => {
+  await loadData()
+})
+</script>
+
+<template>
+  <div class="border rounded-2xl p-3">
+    <div class="flex flex-col gap-3">
+        <h2 class="text-xl">user list</h2>
+      <UTable :data="userListData" class="flex-1" />
+    </div>
+  </div>
+</template>
